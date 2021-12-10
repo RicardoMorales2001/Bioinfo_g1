ef464c80b3db30af80880290b37ef2162be73ee3
RicardoMorales2001 <angel.morales@est.ikiam.edu.ec>
2021-12-10 12:57:11 -0600

    Aquí se corrigió algunas cosas del código y se estableció variables como: n, f, c.

--- netsize.sh
+++ netsize.sh
@@ -1,4 +1,7 @@
 ## Primera parte del ejercico
 $ cd Documents/CSB/CSB/unix/data/Saavedra2013
-$ wc -l n45.txt
-$ head -n 1 n45.txt |tr -d " "| tr -d "\n" | wc -c
+$ wc -l n41.txt
+$ head -n 2 n41.txt |tr -d " "| tr -d "\n" | wc -c
+$ n=n41.txt
+$ f=$(wc -l < n41.txt)
+$ c=$(head -n 2 n41.txt |tr -d " "| tr -d "\n"| wc -c)
