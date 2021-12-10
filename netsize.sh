84795d265adfc5df5ccf079425f07f4bc6a4d111
RicardoMorales2001 <angel.morales@est.ikiam.edu.ec>
2021-12-10 15:21:14 -0600

    Corrección del código

--- netsize.sh
+++ netsize.sh
@@ -1,11 +1,10 @@
 ## Primera parte del ejercico
 $ cd Documents/CSB/CSB/unix/data/Saavedra2013
 $ wc -l n41.txt
-$ head -n 2 n41.txt |tr -d " "| tr -d "\n" | wc -c
-$ n=n41.txt
-$ f=$(wc -l < n41.txt)
-$ c=$(head -n 2 n41.txt |tr -d " "| tr -d "\n"| wc -c)
-$ echo "Nombre=$n" > ../../../../respuesta.txt
-$ echo "Filas=$f" >> ../../../../respuesta.txt
-$ echo "Columnas=$c" >> ../../../../respuesta.txt
+$ numero=n41.txt
+$ fila=$(wc -l < n41.txt)
+$ columna=$(head -n 2 n41.txt |tr -d " "| tr -d "\n"| wc -c)
+$ echo "Nombre=$numero" > ../../../../respuesta.txt
+$ echo "Filas=$fila" >> ../../../../respuesta.txt
+$ echo "Columnas=$columna" >> ../../../../respuesta.txt
 $ cat ../../../../respuesta.txt
