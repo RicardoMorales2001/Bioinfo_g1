3ed3f6e1967fce533ed876b5dce37889c4a0a813
RicardoMorales2001 <angel.morales@est.ikiam.edu.ec>
2021-12-10 15:21:58 -0600

    Correción del código

--- netsize.sh
+++ netsize.sh
@@ -1,6 +1,5 @@
 ## Primera parte del ejercico
 $ cd Documents/CSB/CSB/unix/data/Saavedra2013
-$ wc -l n41.txt
 $ numero=n41.txt
 $ fila=$(wc -l < n41.txt)
 $ columna=$(head -n 2 n41.txt |tr -d " "| tr -d "\n"| wc -c)
