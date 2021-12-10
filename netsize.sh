9c5909ecd8e6b516b49adc896e1cddece71ae1e6
RicardoMorales2001 <angel.morales@est.ikiam.edu.ec>
2021-12-10 12:03:30 -0600

    Se cambió el documento de texto n27 por el n45 por comodidad para trabajar

--- netsize.sh
+++ netsize.sh
@@ -1,4 +1,4 @@
 ## Primera parte del ejercico
 $ cd Documents/CSB/CSB/unix/data/Saavedra2013
-$ WC -l n27.txt
-$ head -n 1 n27.txt |tr -d " "| tr -d "\n" | wc -c
+$ wc -l n45.txt
+$ head -n 1 n45.txt |tr -d " "| tr -d "\n" | wc -c
