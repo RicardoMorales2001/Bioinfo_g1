cdff31b3390f976e27dddf4fee90de912a8a59f7
RicardoMorales2001 <angel.morales@est.ikiam.edu.ec>
2021-12-11 11:39:48 -0600

    Se agregò informacion del código para bucles y se modifico el código fuente

--- netsize_all.sh
+++ netsize_all.sh
@@ -1,1 +1,9 @@
-
+## Segunda parte del ejercico
+#Se declara la variable datos siendo igual a *.txt esto con el fin de que en la variable
+#se encuentren todos los archivos con terminación txt.
+datos=*.txt
+#Usamos el comando echo para imprimir la variable.
+echo $datos
+# Se realiza el uso del codigo para crear bucles con la implementacion del
+# código que se creo en la primera parte.
+for a in $datos; do fila=$(wc -l $a); columna=$(head -n 1 $a | tr -d ' ' | tr -d '\n' | wc -c); echo $a $fila $columna  >> ../../../../netsize_all.txt; done
